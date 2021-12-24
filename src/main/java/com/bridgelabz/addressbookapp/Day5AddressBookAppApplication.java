466d5adb253df9ce222d0ebbdcbfd0da47c070e6
mudable <mudableshilpa@gmail.com>
2021-12-24 16:31:31 +0530

    Created UC1 for Address book spring project

---
+++ src/main/java/com/bridgelabz/addressbookapp/Day5AddressBookAppApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.addressbookapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day5AddressBookAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day5AddressBookAppApplication.class, args);
+	}
+
+}
