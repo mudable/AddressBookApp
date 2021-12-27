8318fff553334ea1e14243d98e42bb4008f8ec5d
mudable <mudableshilpa@gmail.com>
2021-12-27 20:14:59 +0530

    Added : Uc4 to use Lombok which provides the getter and setter

--- src/main/java/com/bridgelabz/addressbookapp/dto/ResponseDTO.java
+++ src/main/java/com/bridgelabz/addressbookapp/dto/ResponseDTO.java
@@ -1,23 +1,13 @@
 package com.bridgelabz.addressbookapp.dto;
 
-public class ResponseDTO {
-    private String message;
-    private Object data;
+import lombok.Data;
 
-    public ResponseDTO(String message,Object data) {
-        this.message = message;
-        this.data = data;
-    }
-    public String getMessage() {
-        return message;
-    }
-    public void setMessage(String message) {
-        this.message = message;
-    }
-    public Object getData() {
-        return data;
-    }
-    public void setData(Object data) {
-        this.data = data;
-    }
+public @Data class ResponseDTO {
+	private String message;
+	private Object data;
+
+	public ResponseDTO(String message, Object data) {
+		this.message = message;
+		this.data = data;
+	}
 }
